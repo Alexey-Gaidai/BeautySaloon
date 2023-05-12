a1707b3a0154d4beb12988168586c07214d957ad
Alexey-Gaidai <1123_666@mail.ru>
2023-05-13 01:06:02 +0700

    добавлены клиенты

--- BeautySaloon/UI/Clients/EditClient.xaml.cs
+++ BeautySaloon/UI/Clients/EditClient.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BeautySaloon.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +20,47 @@ namespace BeautySaloon.UI.Clients
     /// </summary>
     public partial class EditClient : Window
     {
+        private Data.Clients client;
         public EditClient()
         {
             InitializeComponent();
+        }
+
+        public EditClient(Data.Clients _client)
+        {
+            InitializeComponent();
+            client = _client;
+            Init();
+        }
+
+        private void Init()
+        {
+            IDTextBox.Text = client.ID.ToString();
+            NameTextBox.Text = client.Name;
+            PhoneNumberTextBox.Text = client.Phone_Number;
+            NoteTextBox.Text = client.Note;
+        }
+
+        private void EditClient_Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Data.Clients client = AppConnect.SaloonDB.Clients.Find(int.Parse(IDTextBox.Text));
+                client.Name = NameTextBox.Text;
+                client.Phone_Number = PhoneNumberTextBox.Text;
+                client.Note = NoteTextBox.Text;
+                AppConnect.SaloonDB.SaveChanges();
+                this.DialogResult = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Cancel_Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
         }
     }
 }
--- BeautySaloon/UI/Clients/PageClients.xaml.cs
+++ BeautySaloon/UI/Clients/PageClients.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BeautySaloon.Data;
+using BeautySaloon.UI.Masters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +25,102 @@ namespace BeautySaloon.UI.Clients
         public PageClients()
         {
             InitializeComponent();
+            LoadClients();
+        }
+
+        private void LoadClients()
+        {
+            try
+            {
+                ClientsDataGrid.ItemsSource = AppConnect.SaloonDB.Clients.ToList();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void AddClient_Button_Click(object sender, RoutedEventArgs e)
+        {
+            AddClient addClient = new AddClient();
+            bool? result = addClient.ShowDialog();
+
+            if (result == true)
+            {
+                MessageBox.Show("Добавлено!");
+                // обновление data grid view
+                LoadClients();
+            }
+            else
+            {
+                MessageBox.Show("Не добавлено!");
+            }
+        }
+
+        private void EditClient_Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Data.Clients selectedClient = (Data.Clients)ClientsDataGrid.SelectedItem;
+                // проверка на выбор услуги
+                if (selectedClient == null)
+                {
+                    MessageBox.Show("Выберите услугу!");
+                    return;
+                }
+                // открытие окна редактирования услуги
+                EditClient editClient = new EditClient(selectedClient);
+                bool? result = editClient.ShowDialog();
+
+                if (result == true)
+                {
+                    MessageBox.Show("Изменено!");
+                    // обновление data grid view
+                    LoadClients();
+                }
+                else
+                {
+                    MessageBox.Show("Не изменено!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void DeleteClient_Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // поиск записи по id
+                int id;
+                if (ClientsDataGrid.SelectedItems.Count != 0 && ClientsDataGrid.SelectedItems.Count < 2)
+                {
+                    id = (ClientsDataGrid.SelectedItem as Data.Clients).ID;
+                }
+                else
+                {
+                    MessageBox.Show("Выберите клиента!");
+                    return;
+                }
+                var client = AppConnect.SaloonDB.Clients.Find(id);
+                // удаление записи из базы данных
+                AppConnect.SaloonDB.Clients.Remove(client);
+                AppConnect.SaloonDB.SaveChanges();
+                // обновление data grid view
+                LoadClients();
+                MessageBox.Show("Удалено!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            AppFrame.frameMain.GoBack();
         }
     }
 }
